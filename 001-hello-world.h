f9a095bda797386ede5050bb5be2de1e5fc41dee
BobonazarVahobov <bobonazarvahobov@gmail.com>
2021-04-10 23:01:27 +0900

    files updated

--- 001-hello-world.h
+++ 001-hello-world.h
@@ -1,9 +1,8 @@
 #include "iostream"
-
 using namespace std;
 
-int hello(){
-    cout << "Hello, World";
+int helloWorld(){
+    cout << "Hello, World!";
     return 0;
-}
 
+}
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "001-hello-world.h"
+using namespace std;
+
+int main(){
+     helloWorld();
+     return 0;
+}
+
