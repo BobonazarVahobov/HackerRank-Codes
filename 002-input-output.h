5d3e97a08d35a980ce63bfdd1ce6a4eb9f47e68e
BobonazarVahobov <bobonazarvahobov@gmail.com>
2021-04-10 23:08:12 +0900

    002-input-output.h created

---
+++ 002-input-output.h
@@ -0,0 +1,17 @@
+#ifndef HACKERRANK_CODES_002_INPUT_OUTPUT_H
+#define HACKERRANK_CODES_002_INPUT_OUTPUT_H
+
+#include "iostream"
+
+using namespace std;
+
+int inputOutput() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << a + b + c;
+    return 0;
+}
+
+
+#endif //HACKERRANK_CODES_002_INPUT_OUTPUT_H
+
